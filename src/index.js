9ba7a87da2449eec8996835b15b9bb02c6aa5a1d
Bogdddan <bogdankrajnik95@gmail.com>
2023-06-27 12:52:55 +0300

    n

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/Bogdddan'>
+    <BrowserRouter basename='/Bogdddan.github.io'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
